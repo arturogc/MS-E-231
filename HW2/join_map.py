c72afe7153e2f77df315cd1cd442fe6620d3c9f1
Arturo Garrido Contreras <artu_95@hotmail.com>
2019-10-22 10:31:23 -0700

    Finish map function

--- HW2/join_map.py
+++ HW2/join_map.py
@@ -17,15 +17,20 @@ for line in sys.stdin:
 
 	
 	# The line may be from either table (fares.csv or trip.csv), so we have to identify it:
-	if len(params) == 14: # trip data
-        key = ",".join([params[0],params[1],params[5]])
+	# From the trips table
+	if len(params) == 14:
+        #key = ",".join([params[0],params[1],params[5]])
+        key = params[0] + "," + params[5]
+    # From the fare table
     elif len(params) == 11: # fare data
-        key = ",".join([params[0],params[1],params[3]])
+        #key = ",".join([params[0],params[1],params[3]])
+        key = params[0] + "," + params[3]
     else:
-        key = "NA" # this get checked and thrown out in the
+        key = "NA"
 
-    # If it is the header, we get rid of it (not needed for the reduce step)
+    # We now print the result, unless it is a header!
     if key[0].isdigit():
+    	# Print the key and the value, with a tab in between
         print(key + "\t" + line.strip())
     else:
         pass
