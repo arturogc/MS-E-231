ff39002902c157eed581328e47db71c02dc2db23
Arturo Garrido Contreras <arturo@Arturos-MacBook-Pro.local>
2019-10-25 15:26:53 -0700

    Improve join map-reduce

--- HW2/join_map.py
+++ HW2/join_map.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Step 3
 # Map step
 
@@ -11,26 +12,27 @@ For each line of data, it emits the following (key,value) pais:
 
 """
 
-for line in sys.stdin:
-	# We input the first line
-	params = line.strip().split(,)
+def main():
+    for line in sys.stdin:
+    	# We input the first line
+        params = line.strip().split(",")
+    	
+    	# The line may be from either table (fares.csv or trip.csv), so we have to identify it:
+    	# From the trips table
+        if len(params) == 14: # trip data
+            key = params[0] + "," + params[5]
+        # From the fare table
+        elif len(params) == 11: # fare data
+            key = params[0] + "," + params[3]
+        else:
+            key = "NA"
 
-	
-	# The line may be from either table (fares.csv or trip.csv), so we have to identify it:
-	# From the trips table
-	if len(params) == 14:
-        #key = ",".join([params[0],params[1],params[5]])
-        key = params[0] + "," + params[5]
-    # From the fare table
-    elif len(params) == 11: # fare data
-        #key = ",".join([params[0],params[1],params[3]])
-        key = params[0] + "," + params[3]
-    else:
-        key = "NA"
+        # We now print the result, unless it is a header!
+        if key[0].isdigit():
+        	# Print the key and the value, with a tab in between
+            print(key + "\t" + line.strip())
+        else:
+            pass
 
-    # We now print the result, unless it is a header!
-    if key[0].isdigit():
-    	# Print the key and the value, with a tab in between
-        print(key + "\t" + line.strip())
-    else:
-        pass
+if __name__ == "__main__":
+    main()
--- HW2/join_reduce.py
+++ HW2/join_reduce.py
@@ -1,95 +1,105 @@
-
+#!/usr/bin/env python3
 # Step 3
 # Reduce step
 
 """
 
-Purpose: Reducer to take a sorted list of trip and fare data lines and join them
-into a single line of data with all unique data values.
+Purpose: join the trip and fare lines into a single line of data.
 
 """
-from datetime import datetime
 from itertools import groupby
 from operator import itemgetter
 import sys
+from datetime import datetime
 
-# Constants for data filtering. *_IDX indicates index in the data list
-# trip constants
+
+# Define the indexes for each column in both the trip and fare data
+# in order to make the code more flexible
+# Trip data column index
 TRIPTIME      = 60
-PICKUP_TIME_IDX = 5
-DROPOFF_TIME_IDX = 6
-TRIPTIME_IDX  = 8
-TRIP_DIST_IDX = 9
-PICK_LONG_IDX = 10
-PICK_LATT_IDX = 11
-DROP_LONG_IDX = 12
-DROP_LATT_IDX = 13
+PICKUP_TIME = 5
+DROPOFF_TIME = 6
+TRIPTIME  = 8
+TRIP_DIST = 9
+PICK_LONG = 10
+PICK_LATT = 11
+DROP_LONG = 12
+DROP_LATT = 13
 
-#fare constants
-FARE_IDX         = 5
-TOTAL_AMOUNT_IDX = 10
+# Fare data columns
+PAYMENT         = 5
+TOTAL_AMOUNT = 10
 
 FORMAT = "%Y-%m-%d %H:%M:%S"
 
-def read_mapper_output(lines):
-    """Returns generator over each line of lines as a list split by tabs."""
+def read_mapper_output(lines, separator = '\t'):
     for line in lines:
-        #print(line.rstrip().split('\t', 1))
-        yield line.rstrip().split('\t', 1)
+        yield line.rstrip().split(separator, 1)
 
 
-"""Take lines from stdin and join the trip and fare data lines.
-   Do a JOIN, where `trip` is left and 'fare' is right
-"""
-data = read_mapper_output(sys.stdin)
-for key, group in groupby(data, itemgetter(0)):
-    # [Mine] In every group we have a line from the "trip" table
-    # and a line from the "fare" table. We assign them to different
-    # sets
+def main():
+    # Input comes from STDIN
+    data = read_mapper_output(sys.stdin)
+    for key, group in groupby(data, itemgetter(0)):
+    
+        # In every group we have a line from the "trip" table
+        # and a line from the "fare" table. We assign them to different
+        # sets
+    
+        trip = []
+        fare = []
+    
+        for key, ride_line in group:
+            ride_params = ride_line.strip().split(",")
+            if len(ride_params) == 14: # trip data
+                trip = ride_params
+                # We create a datetime object, and in case of fail, get rid of the line
+                try:
+                    dropoff_time = datetime.strptime(trip[DROPOFF_TIME], FORMAT)
+                    pickup_time = datetime.strptime(trip[PICKUP_TIME], FORMAT)
+                except:
+                    trip = []
+            elif len(ride_params) == 11:
+                fare = ride_params
+            else:
+                pass
 
-    trip = []
-    fare = []
+	    # Get rid of the data that is obviously corrupt
+        try:
+	        # Don't write the line if either trip or fare is missing,
+	        # or if it is the header
 
-    for key, ride_data in group:
-        ride_data_list = ride_data.strip().split(",")
-        if len(ride_data_list) == 14: # trip data
-            trip = ride_data_list
-            # if we can't create a good datetime object, get rid of the data
-            try:
-                dropoff_time = datetime.strptime(trip[DROPOFF_TIME_IDX], FORMAT)
-                pickup_time = datetime.strptime(trip[PICKUP_TIME_IDX], FORMAT)
-            except:
-                trip = []
-        elif len(ride_data_list) == 11:
-            fare = ride_data_list
-        else:
-            pass
+	        # TODO: set these thresholds
+	        # Corrupt GPS pickup data
+	        # Corrupt GPS dropoff data
+	        # Free ride
+	        # Too long or too short trips
+	        # Unreasonably long or short trips
+            if trip == [] or fare == [] or trip[0] == "medallion":
+                pass
 
-    # try to filter the data. If there is anything wrong with it here,
-    # just get rid of it
-    try:
-        # Make sure there are two data lines and get ride of the header
-        if trip == [] or fare == [] or trip[0] == "medallion":
-            pass
+            # filter out obvious errors: trips too short or long, bad GPS data,
+            # no fare, trips over 2 hours (7200 sec) or under 10 seconds
+            # Similar filters as [1]
+            elif float(trip[TRIP_DIST]) <= 0.001 \
+            or float(trip[TRIP_DIST]) >= 50 \
+            or float(trip[PICK_LATT]) == 0 \
+            or float(trip[PICK_LONG]) == 0 \
+            or float(trip[DROP_LATT]) == 0 \
+            or float(trip[DROP_LONG]) == 0 \
+            or float(fare[PAYMENT] == 0) \
+            or float(fare[TOTAL_AMOUNT]) == 0 \
+            or (dropoff_time - pickup_time).total_seconds() >= 7200 \
+            or (dropoff_time - pickup_time).total_seconds() < 10:
+                pass
 
-        # filter out obvious errors: trips too short or long, bad GPS data,
-        # no fare, trips over 2 hours (7200 sec) or under 10 seconds
-        # Similar filters as [1]
-        elif float(trip[TRIP_DIST_IDX]) <= 0.001 \
-        or float(trip[TRIP_DIST_IDX]) >= 50 \
-        or float(trip[PICK_LATT_IDX]) == 0 \
-        or float(trip[PICK_LONG_IDX]) == 0 \
-        or float(trip[DROP_LATT_IDX]) == 0 \
-        or float(trip[DROP_LONG_IDX]) == 0 \
-        or float(fare[FARE_IDX] == 0) \
-        or float(fare[TOTAL_AMOUNT_IDX]) == 0 \
-        or (dropoff_time - pickup_time).total_seconds() >= 7200 \
-        or (dropoff_time - pickup_time).total_seconds() < 10:
-            pass
+            else:
+                print("\t".join(trip + fare[4:]))
+        except:
+            #print("ERRRRRRRRRRRROOOOOOOOOOORRRRRRRRRRRR!")
+            pass # get rid of the data
 
-        else:
-            print("\t".join(trip + fare[4:]))
-    except:
-        #print("ERRRRRRRRRRRROOOOOOOOOOORRRRRRRRRRRR!")
-        pass # get rid of the data
+
+if __name__ == "__main__":
+    main()
 
